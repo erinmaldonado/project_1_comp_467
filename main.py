1d27c8615ab52e1288d5264def21fc47d31b28e3
Erin Maldonado <erinjmaldonado@gmail.com>
2024-03-07 14:01:01 -0800

    removed [baselightfilesystem] from filename to find folder in workorder. split filename from frames.

--- main.py
+++ main.py
@@ -1,42 +1,19 @@
+from ast import parse
+
 import Parse
 
 
 def parse_baselight(data):
-    file = data.remove('/baselight')
-    print(data)
+    split_data = data.replace("/baselightfilesystem1/", "")
+    final = split_data.split(' ')
+    print(final)
     """
         data_parsed = Parse(file, frames)
     """
 
-def get_baselight():
-    file = open('Baselight_export.txt')
-    lines = file.readlines()
-    file.close()
-    parse_baselight(lines)
-
-
-def get_Xytech():
-    file = open('Xytech.txt')
-    lines = file.readlines()
-    file.close()
-    parse_Xytech(lines)
-
-
-def parse_Xytech(lines):
-    for line in lines:
-        print(line)
-
-
-def parse_baselight(lines):
-    for line in lines:
-        separate = line.split(' ', 1)
-        file_path = separate[0].split('/')
-        nums = separate[1] if len(separate) > 1 else ''
-        print(file_path)
-        print(nums)
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    get_baselight()
-    get_Xytech()
+    raw_data = "/baselightfilesystem1/Dune2/reel1/partA/1920x1080 2 3 4 31 32 33 67 68 69 70 122 123 155 1023 1111 ' \
+              '1112 1160 1201 1202 1203 1204 1205 1211 1212 1213 1214 1215"
+    parse_baselight(raw_data)
